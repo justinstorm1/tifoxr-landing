0b76b3c66a98dfd905732d7857987dc87df41bad
justinstorm1 <justinstorm1@gmail.com>
2025-01-21 20:54:37 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+const copyright = document.getElementById('copyright');
+const date = new Date();
+const year = date.getFullYear();
+copyright.innerHTML = `Website Designed by Justin Storm<br>Copy &copy; All Rights Reserved<br>TifoXR - ${year}`;
